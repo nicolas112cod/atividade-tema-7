dc506b3a1137ca2ea1cccd4db5319f01fc33fd46
nicolas112cod <nicolasbarsoti.nb@gmail.com>
2024-07-01 23:21:56 -0300

    Add files via upload

---
+++ Livro.java
@@ -0,0 +1,15 @@
+public class Livro {
+
+    String nome;
+    String genero;
+    double ano_de_lançamento;
+
+    Livro (String n, String g, double a){
+        nome=n;
+        genero=g;
+        ano_de_lançamento=a;
+        
+    }
+
+    
+}
